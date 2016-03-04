75422ffbc6892641deffcd0fc543f5f34226148f
Zaid <e0013259@u.nus.edu>
2016-03-05 04:19:15 +0800

    Allow Import To Eclips

--- src/edu/nus/iss/pos/core/ApplicationLauncher.java
+++ src/edu/nus/iss/pos/core/ApplicationLauncher.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.nus.iss.pos.core;
 
 import edu.nus.iss.pos.core.dao.IUnitOfWork;
